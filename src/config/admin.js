0184579a783658e0ec156d75101b13772d1cdae6
Malik Mahmud <almalikmahmud@gmail.com>
2019-11-20 22:34:03 +0100

    Add babel config

---
+++ src/config/admin.js
@@ -0,0 +1,12 @@
+import admin from 'firebase-admin';
+
+admin.initializeApp({
+  credential: admin.credential.cert({
+    projectId: process.env.PROJECT_ID,
+    clientEmail: process.env.CLIENT_EMAIL,
+    privateKey: process.env.PRIVATE_KEY.replace(/\\n/g, '\n'),
+  }),
+  databaseURL: process.env.DB_URI,
+});
+
+export default admin;
