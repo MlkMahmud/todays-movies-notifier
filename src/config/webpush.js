649ecaf2b0d6a3ac87f044c80e87d254f4746e71
Malik Mahmud <almalikmahmud@gmail.com>
2019-11-22 12:07:47 +0100

    Correct vapid subject url

--- src/config/webpush.js
+++ src/config/webpush.js
@@ -1,7 +1,7 @@
 import webPush from 'web-push';
 
 webPush.setVapidDetails(
-  process.env.CLIENT_EMAIL,
+  process.env.VAPID_SUBJECT,
   process.env.PUBLIC_VAPID_KEY,
   process.env.PRIVATE_VAPID_KEY,
 );
